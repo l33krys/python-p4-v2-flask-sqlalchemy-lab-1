2f77e80cda1618c7b6d87e49de24258eade2d586
Krystle Lee <l33.krys@gmail.com>
2023-10-03 21:06:17 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -20,7 +20,31 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+
+    magnitudes = []
+
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        magnitudes.append(earthquake.to_dict())
+    
+    body = {'count': len(magnitudes),
+            'quakes': magnitudes
+            }
+    return make_response(body, 200)
 
 
 if __name__ == '__main__':
